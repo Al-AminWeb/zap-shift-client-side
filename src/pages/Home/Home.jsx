f6143cedfdcad47ebc457e46df2987bceca6232b
Al-AminWeb <143194944+Al-AminWeb@users.noreply.github.com>
2025-06-24 01:37:05 +0600

    create a services section using chatgpt

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from "./banner/Banner.jsx";
+import Services from "./services/Services.jsx";
 
 const Home = () => {
     return (
         <div>
            <Banner/>
+            <Services/>
         </div>
     );
 };
