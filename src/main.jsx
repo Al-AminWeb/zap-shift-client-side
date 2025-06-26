c7c1138a32dd0822ab7b6249b0a2271a37a38496
Al-AminWeb <143194944+Al-AminWeb@users.noreply.github.com>
2025-06-26 20:24:50 +0600

    private route added

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ import {router} from "./router/router.jsx";
 
 import 'aos/dist/aos.css';
 import AOS from 'aos';
-import {AuthContext} from "./context/AuthContext/AuthContext.jsx";
 import AuthProvider from "./context/AuthContext/AuthProvider.jsx";
 
 AOS.init()
@@ -17,9 +16,9 @@ createRoot(document.getElementById('root')).render(
     <StrictMode>
         <div className='bg-[#ebeced]'>
             <div className='urbanist max-w-7xl mx-auto '>
-               <AuthProvider>
-                   <RouterProvider router={router}/>
-               </AuthProvider>
+                <AuthProvider>
+                    <RouterProvider router={router}/>
+                </AuthProvider>
             </div>
         </div>
 
